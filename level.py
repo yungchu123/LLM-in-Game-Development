2247ddc17dd92dc4f92b060a642b67f817803a9f
yungchu <linyungchu@gmail.com>
2025-01-16 15:35:40 +0800

    add overlay for tools and seeds

--- level.py
+++ level.py
@@ -1,6 +1,7 @@
 import pygame 
 from settings import *
 from player import Player
+from overlay import Overlay
 
 class Level:
     def __init__(self):
@@ -15,8 +16,11 @@ class Level:
 
     def setup(self):
         self.player = Player((640,360), self.all_sprites)
+        self.overlay = Overlay(self.player)
         
     def run(self,dt):
         self.display_surface.fill('black')
         self.all_sprites.draw(self.display_surface)
         self.all_sprites.update(dt) # Calls update() on all children
+        
+        self.overlay.display()
