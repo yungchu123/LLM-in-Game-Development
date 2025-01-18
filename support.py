23d5a4f659f37a97f9a793769aa5c776057848b7
yungchu <linyungchu@gmail.com>
2025-01-18 20:40:50 +0800

    add graphic logic for soil tiling

--- support.py
+++ support.py
@@ -1,6 +1,7 @@
 from os import walk
 import pygame
 
+# when the order of image surface matters
 def import_folder(path):
     surface_list = []
 
@@ -11,3 +12,15 @@ def import_folder(path):
             surface_list.append(image_surface)
             
     return surface_list
+
+# when we need key values to access image surface
+def import_folder_dict(path):
+    surface_dict = {}
+
+    for _, __, img_files in walk(path):
+        for image in img_files:
+            full_path = path + '/' + image
+            image_surf = pygame.image.load(full_path).convert_alpha()
+            surface_dict[image.split('.')[0]] = image_surf
+
+    return surface_dict
